f6b7d6f3cce81e7fc7d773c433da1b64764a9b2b
danskibum <ger19007@byui.edu>
2021-01-09 20:53:59 -0800

    try3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
- const options = {weekday: 'long', day: "numeric", month: 'long', year: 'numeric'};
- document.getelementbyid('date2').textcontent = new date().tolocaledatestring('en-us', options);
- 
+const options = {weekday: 'long', day: 'numeric', month: "long", year: 'numeric'};
+document.getelementbyid('date2').textcontent = new date().tolocaledatestring('en-us', options);
+let date2 = new date(document.lastModified);
+
+
+
