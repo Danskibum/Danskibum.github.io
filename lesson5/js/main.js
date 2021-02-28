407841a050f900ffb8a8758014b0b671f934eb72
danskibum <ger19007@byui.edu>
2021-02-27 20:52:14 -0800

    storm form

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -4,3 +4,4 @@ document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString
 function toggleMenu() {
     document.getElementsByClassName("menu")[0].classList.toggle('responsive');
 }
+new Date().getDay() == 5 ? document.getElementById("reminder").innerHTML = "Saturday = Preston pancakes in the park! 9:00 a.m. Saturday at the city park pavilion." : banner.style.display = "none";
