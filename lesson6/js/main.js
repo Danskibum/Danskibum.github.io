606cb6561ff3be9f32a011c7bf715c10474ad39b
danskibum <ger19007@byui.edu>
2021-03-03 21:47:23 -0800

    windchill

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -1,6 +1,9 @@
 //document.getElementById("date").innerHTML = document.lastModified;
 const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
 document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString( 'en-us', options);
+
 function toggleMenu() {
     document.getElementsByClassName("menu")[0].classList.toggle('responsive');
 }
+
+new Date().getDay() == 5 ? document.getElementById("reminder").innerHTML = "Saturday = Preston pancakes in the park! 9:00 a.m. Saturday at the city park pavilion." : banner.style.display = "none";
